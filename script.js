72d7292fb5d74ae796b7717f4d6f2c1f4eb4c868
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-05-12 09:47:23 +0530

    all files

---
+++ script.js
@@ -0,0 +1,44 @@
+const video = document.getElementById('video');
+var x='';
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/models')
+    ]);
+
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    );
+}
+document.getElementById('button').addEventListener('click', () =>{
+    startVideo();
+    document.getElementById('video').style.display = 'block';
+    document.getElementById('main').style.display = 'none';
+    x=document.getElementById("name").value;
+})
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height};
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () =>{
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
+        const resizedDetections = faceapi.resizeResults(detections,displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        resizedDetections.forEach( detection => {
+            const box = detection.detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: x + " " + (Math.round(detection.age)) + " year old " + detection.gender })
+            drawBox.draw(canvas)
+          })
+
+    }, 500)
+})
